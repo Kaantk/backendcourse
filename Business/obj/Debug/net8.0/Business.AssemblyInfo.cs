6bf135800f9f7a443fcb2c4f8cf8dbf3daed03fe
Kaan Tetik <kaan.tk3@hotmail.com>
2024-08-05 13:18:26 +0300

    workshop oncesi son degisiklikler

--- Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
+++ Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41ca42be443062be935307f6eead4ac72298d8b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8734d461c3f6b1746d8e0e61135a4bb691a7bff0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
